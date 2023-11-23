506e9cb59910aec0e49a34cd743c40dbbf98cc48
Kevin Bian <kaiwenbian107@gmail.com>
2023-11-23 10:59:56 -0800

    data structure fixed

--- mllmes/data/pecher2012/Orientation/convert_to_jpg.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for file in ./*; do
-    base_name=$(basename "$file")
-    new_name="${base_name%.*}.pict"
-    cp "$file" "$new_name"
-    convert "$new_name" "${new_name%.*}.jpg"
-done
